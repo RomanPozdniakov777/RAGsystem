2255556308cd4007d3cecc579bd140e935f652f1
Vito Skaletta <mafioznikvitoskaletta@gmail.com>
2025-08-05 17:07:27 +0300

    Модель выдает осмысленные ответы + осталось немного совсем

--- rag_generator.py
+++ rag_generator.py
@@ -9,8 +9,14 @@ logging.basicConfig(
     encoding = 'utf-8',
 )
 
+fgfgfg = "ai-forever/FRED-T5-large"
+fgf = "CausalNLP/gpt2-hf_multilingual-70"
+rtr = "ai-forever/rugpt3medium_based_on_gpt2"
+asd = 'google/mt5-small'
+dgfh = 'cointegrated/rut5-base-multitask'
+
 class Generator:
-    def __init__(self, model_name = "ai-forever/FRED-T5-large"):
+    def __init__(self, model_name = "Qwen/Qwen3-0.6B"):
         '''
 
         :param model_name:
@@ -26,7 +32,8 @@ class Generator:
         '''
         try:
             self.generator = pipeline(
-                 "text2text-generation",
+               #"text2text-generation",
+                "text-generation",
                 model = self.model_name,
                 torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32,
                 device_map="auto" if torch.cuda.is_available() else None,
@@ -64,18 +71,25 @@ class Generator:
             #         Ответ:
             #         """
 
-            prompt = f"Ответь на вопрос '{query}', используя информацию: {context[:1000]}"
+            # prompt = f"Ответь на русском языке на вопрос '{query}', используя следующую информацию:\n{context}\n\nОтвет на русском языке:"
+            prompt = f"Вопрос: {query}\nКонтекст: {context}\nОтвет на русском языке:"
 
             response = self.generator(
                 prompt,
                 max_new_tokens = 150,
-                # temperature = 0.7,
-                # top_p = 0.9,
+                temperature = 0.7,
+                top_p = 0.9,
                 repetition_penalty = 1.2,
                 do_sample = True
             )
 
-            answer = response[0]['generated_text']
+            # answer = response[0]['generated_text'].strip()
+
+            full_text = response[0]['generated_text']
+            answer = full_text[len(prompt):].strip()
+
+            if not answer:
+                answer = full_text.strip()
 
             logging.info('Ответ сгенерирован успешно')
 
@@ -84,3 +98,15 @@ class Generator:
         except Exception as e:
             logging.error(f'Ошибка при генерации ответа: {str(e)}')
             raise
+    # def generate_answer(self, query, relevant_chunks):
+    #     '''
+    #
+    #     '''
+    #     if not relevant_chunks:
+    #         return "Извините, не найдено релевантной информации."
+    #
+    #     answer = "Найденная информация:\n\n"
+    #     for i, chunk in enumerate(relevant_chunks[:2]):
+    #         answer += f"{i + 1}. {chunk.strip()}\n\n"
+
+    #       return answer
